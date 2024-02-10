d8fb49af9006ae0e3cef24cabffd346197114acd
Johny-UA <150531628+Johny-UA@users.noreply.github.com>
2024-02-10 20:07:19 +0100

    last task
    правда ненормально?

--- hwk.py
+++ hwk.py
@@ -1,6 +1,22 @@
-num = '1,2,3,'
+info = {1 :{'name': 'Ivan',
+         'age': 35,
+         'lastname': 'Wolf'},
+         2: {'name': 'Anna',
+          'age': 27,
+          'lastname': 'Fish'},
+         3: {'name': 'Tom',
+          'age': 45,
+          'lastname': 'Cave'}
+}
 
-def test(item):
-   return item.split(",");
+text = {'name': 'Ivan',}
 
-print(test(num))
+result = ()
+
+for key, i in info.items():
+   for z, value in i.items():
+      for x, words in text.items():
+         if words == value:
+            if info[key] not in result:
+               result += (info[key],)
+print(result)
